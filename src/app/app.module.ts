ec23884b93815d918889c9a8b5bd8cfbe09dd422
Jonah Elbaz <coach.track.app@gmail.com>
2018-02-23 02:05:00 -0500

    Initialize project without head folder

---
+++ src/app/app.module.ts
@@ -0,0 +1,50 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+
+import { AboutPage } from '../pages/about/about';
+import { ContactPage } from '../pages/contact/contact';
+import { HomePage } from '../pages/home/home';
+import { TabsPage } from '../pages/tabs/tabs';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import {IonicStorageModule} from "@ionic/storage";
+
+import {AngularFireModule} from 'angularfire2';
+import {AngularFireDatabaseModule} from "angularfire2/database";
+import {FIREBASE_CONFIG} from "./firebase-credentials";
+import {AngularFireAuthModule} from "angularfire2/auth";
+
+@NgModule({
+  declarations: [
+    MyApp,
+    AboutPage,
+    ContactPage,
+    HomePage,
+    TabsPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot(),
+    AngularFireModule.initializeApp(FIREBASE_CONFIG),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    AboutPage,
+    ContactPage,
+    HomePage,
+    TabsPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
---
+++ src/app/firebase-credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBuNF5meA-5gA0leHbyd3UsZUqbPCWNo5Y",
+  authDomain: "mastercoach-c4d2b.firebaseapp.com",
+  databaseURL: "https://mastercoach-c4d2b.firebaseio.com",
+  projectId: "mastercoach-c4d2b",
+  storageBucket: "",
+  messagingSenderId: "736172868611"
+};
